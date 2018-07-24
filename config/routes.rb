3f8f2b7d5ce3fff2f3c0e5f0f84c7b29512b9a64
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-24 19:43:43 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'application#current_user'
+    root 'application#hello'
   get 'sessions/new'
   # get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
